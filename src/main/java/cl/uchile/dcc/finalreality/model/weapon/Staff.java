cbc1c88efac1199552bb05a25c4f787de22857aa
Nc-Maxt <m.f.nunez.caceres@gmail.com>
2022-09-27 10:17:20 -0300

    re added methods: Equals, Hashcode, getters. Changed constructor and method to string in Staff Class

--- src/main/java/cl/uchile/dcc/finalreality/model/weapon/Staff.java
+++ src/main/java/cl/uchile/dcc/finalreality/model/weapon/Staff.java
@@ -2,19 +2,68 @@ package cl.uchile.dcc.finalreality.model.weapon;
 
 import java.util.Objects;
 
-public class Staff extends AbstractWeapon{
-    private final String type = "Staff";
+public class Staff implements Weapons{
+    private final String name;
+    private final int damage;
+    private final int weight;
 
     /**
      * Creates a weapon with a name, a base damage, speed, and it's type.
      */
     public Staff(final String name, final int damage, final int weight) {
-        super(name, damage, weight);
+        this.name = name;
+        this.damage = damage;
+        this.weight = weight;
+    }
+    /**
+     * Returns the name of the weapon.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Returns the damage of the weapon.
+     */
+    public int getDamage() {
+        return damage;
+    }
+
+    /**
+     * Returns the weight of the weapon.
+     */
+    public int getWeight() {
+        return weight;
     }
 
     @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(this.getClass() == obj.getClass())) {
+            return false;
+        }
+        Staff other = (Staff) obj;
+        return hashCode() == other.hashCode()
+                && damage == other.getDamage()
+                && weight == other.getWeight()
+                && name.equals(other.getName());
+    }
+
+    /**
+     * Returns the hashcode of the weapon.
+     */
+    public int hashCode() {
+
+        return Objects.hash(Sword.class, name, damage, weight);
+    }
+
+    /**
+     * Returns the weapon and it´s data in a String format.
+     */
     public String toString() {
-        String fp = super.toString();
-        return type+fp;
+        return "Staff{name='%s', damage=%d, weight=%d}"
+                .formatted(name, damage, weight);
     }
 }
