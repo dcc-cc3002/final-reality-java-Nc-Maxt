ce8f40d4a45f25c0e3b5462e81567c67d5eecbb6
Nc-Maxt <m.f.nunez.caceres@gmail.com>
2022-09-27 10:24:26 -0300

    Eliminated Classes:Weapon and Weapon Type

--- src/main/java/cl/uchile/dcc/finalreality/model/character/player/PlayerCharacter.java
+++ src/main/java/cl/uchile/dcc/finalreality/model/character/player/PlayerCharacter.java
@@ -9,7 +9,6 @@ package cl.uchile.dcc.finalreality.model.character.player;
  */
 
 import cl.uchile.dcc.finalreality.model.character.GameCharacter;
-import cl.uchile.dcc.finalreality.model.weapon.Weapon;
 import cl.uchile.dcc.finalreality.model.weapon.Weapons;
 
 /**
