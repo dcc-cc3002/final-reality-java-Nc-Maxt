441638e4aefe1139661ad3c3b111f6e0d673bc32
Nc-Maxt <m.f.nunez.caceres@gmail.com>
2022-09-27 09:44:35 -0300

    changed type from Weapon (Class) to Weapons Interface, and create a new Knife instance

--- src/main/java/cl/uchile/dcc/TimerExample.java
+++ src/main/java/cl/uchile/dcc/TimerExample.java
@@ -4,8 +4,9 @@ import cl.uchile.dcc.finalreality.exceptions.InvalidStatValueException;
 import cl.uchile.dcc.finalreality.model.character.GameCharacter;
 import cl.uchile.dcc.finalreality.model.character.player.AbstractPlayerCharacter;
 import cl.uchile.dcc.finalreality.model.character.player.Thief;
-import cl.uchile.dcc.finalreality.model.weapon.Weapon;
-import cl.uchile.dcc.finalreality.model.weapon.WeaponType;
+import cl.uchile.dcc.finalreality.model.weapon.Knife;
+import cl.uchile.dcc.finalreality.model.weapon.Weapons;
+
 import java.util.Random;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
@@ -21,7 +22,7 @@ public class TimerExample {
     Random rng = new Random();
     for (int i = 0; i < 10; i++) {
       // Gives a random speed to each character to generate different waiting times
-      var weapon = new Weapon("", 0, rng.nextInt(50), WeaponType.KNIFE);
+      Weapons weapon = new Knife("", 0, rng.nextInt(50));
       var character = new Thief(Integer.toString(i), 10, 10, queue);
       character.equip(weapon);
       character.waitTurn();
