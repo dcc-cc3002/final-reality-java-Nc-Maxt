cc8022684e3bdf0c525306061e6c62c6361c014d
Nc-Maxt <m.f.nunez.caceres@gmail.com>
2022-09-26 23:06:33 -0300

    added methods and constructors to weapons

--- src/main/java/cl/uchile/dcc/finalreality/model/weapon/Axe.java
+++ src/main/java/cl/uchile/dcc/finalreality/model/weapon/Axe.java
@@ -1,4 +1,64 @@
 package cl.uchile.dcc.finalreality.model.weapon;
 
+import java.util.Objects;
+
 public class Axe {
+    private final String name;
+    private final int damage;
+    private final int weight;
+    private final String type = "Axe";
+
+    /**
+     * Creates a weapon with a name, a base damage, speed, and it's type.
+     */
+    public Axe(final String name, final int damage, final int weight) {
+        this.name = name;
+        this.damage = damage;
+        this.weight = weight;
+    }
+
+    private String getName() {
+        return name;
+    }
+
+    private int getDamage() {
+        return damage;
+    }
+
+    /**
+     * Returns the weight of the weapon.
+     */
+    public int getWeight() {
+        return weight;
+    }
+
+    private String getType() {
+        return type;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof final Axe weapon)) {
+            return false;
+        }
+        return hashCode() == weapon.hashCode()
+                && damage == weapon.damage
+                && weight == weapon.weight
+                && name.equals(weapon.name)
+                && type == weapon.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(Axe.class, name, damage, weight, type);
+    }
+
+    @Override
+    public String toString() {
+        return "Weapon{name='%s', damage=%d, weight=%d, type=%s}"
+                .formatted(name, damage, weight, type);
+    }
 }
