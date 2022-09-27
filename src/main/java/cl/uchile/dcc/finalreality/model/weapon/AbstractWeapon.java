8fd8589f5c37348b167a591c26cb85f050db86ef
Nc-Maxt <m.f.nunez.caceres@gmail.com>
2022-09-27 01:01:42 -0300

    added documentation in AbstractWeapons

--- src/main/java/cl/uchile/dcc/finalreality/model/weapon/AbstractWeapon.java
+++ src/main/java/cl/uchile/dcc/finalreality/model/weapon/AbstractWeapon.java
@@ -3,23 +3,35 @@ package cl.uchile.dcc.finalreality.model.weapon;
 import java.util.Objects;
 
 public abstract class AbstractWeapon implements Weapons {
-    private final String name;
     private final int damage;
     private final int weight;
+    private final String name;
 
     /**
-     * Creates a weapon with a name, a base damage, speed, and it's type.
+     * Creates a new Weapon.
+     * This constructor is <b>protected</b>, because it'll only be used by subclasses.
+     *
+     * @param name
+     *     the weapon's name
+     * @param damage
+     *     the weapon's max hp
+     * @param weight
+     *     the weapon's defense
      */
-    public AbstractWeapon(final String name, final int damage, final int weight) {
+    protected AbstractWeapon(final String name, final int damage, final int weight) {
         this.name = name;
         this.damage = damage;
         this.weight = weight;
     }
-
+    /**
+     * Returns the name of the weapon.
+     */
     public String getName() {
         return name;
     }
-
+    /**
+     * Returns the damage of the weapon.
+     */
     public int getDamage() {
         return damage;
     }
@@ -31,7 +43,9 @@ public abstract class AbstractWeapon implements Weapons {
         return weight;
     }
 
-
+    /**
+     * Returns a boolean depending on if the 2 instances are the same or not.
+     */
     @Override
     public boolean equals(final Object o) {
         if (this == o) {
@@ -46,15 +60,19 @@ public abstract class AbstractWeapon implements Weapons {
                 && weight == other.getWeight()
                 && name.equals(other.getName());
     }
-
+    /**
+     * Returns the hashcode of the weapon.
+     */
     @Override
     public int hashCode() {
         return Objects.hash(this.getClass(), name, damage, weight);
     }
-
+    /**
+     * Returns the weapon without it´s type in a String format.
+     */
     @Override
     public String toString() {
-        return "Weapon{name='%s', damage=%d, weight=%d}"
+        return "{name='%s', damage=%d, weight=%d}"
                 .formatted(name, damage, weight);
     }
 }
