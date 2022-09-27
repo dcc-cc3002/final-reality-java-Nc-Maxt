f94d8dfd116e30cadf2d8efe535dc0291bc3c818
Nc-Maxt <m.f.nunez.caceres@gmail.com>
2022-09-26 23:52:31 -0300

    New Interface Weapons

---
+++ src/main/java/cl/uchile/dcc/finalreality/model/weapon/Weapons.java
@@ -0,0 +1,18 @@
+package cl.uchile.dcc.finalreality.model.weapon;
+
+/**
+ * A class that holds all the information of a weapon.
+ *
+ * @author <a href="https://www.github.com/r8vnhill">R8V</a>
+ * @author ~Your name~
+ */
+
+public interface Weapons {
+    void getName();
+
+    void getDamage();
+
+    void getWeight();
+
+    void getType();
+}
