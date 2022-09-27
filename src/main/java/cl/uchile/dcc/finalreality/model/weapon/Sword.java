fccb4e94e311c5a85fff231796bbb462f580457b
Nc-Maxt <m.f.nunez.caceres@gmail.com>
2022-09-27 01:28:52 -0300

    Eliminated methods in Sword class:equals, getters and hashcode

--- src/main/java/cl/uchile/dcc/finalreality/model/weapon/Sword.java
+++ src/main/java/cl/uchile/dcc/finalreality/model/weapon/Sword.java
@@ -2,63 +2,19 @@ package cl.uchile.dcc.finalreality.model.weapon;
 
 import java.util.Objects;
 
-public class Sword {
-    private final String name;
-    private final int damage;
-    private final int weight;
+public class Sword extends AbstractWeapon{
     private final String type = "Sword";
 
     /**
      * Creates a weapon with a name, a base damage, speed, and it's type.
      */
     public Sword(final String name, final int damage, final int weight) {
-        this.name = name;
-        this.damage = damage;
-        this.weight = weight;
-    }
-
-    private String getName() {
-        return name;
-    }
-
-    private int getDamage() {
-        return damage;
-    }
-
-    /**
-     * Returns the weight of the weapon.
-     */
-    public int getWeight() {
-        return weight;
-    }
-
-    private String getType() {
-        return type;
-    }
-
-    @Override
-    public boolean equals(final Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof final Sword weapon)) {
-            return false;
-        }
-        return hashCode() == weapon.hashCode()
-                && damage == weapon.damage
-                && weight == weapon.weight
-                && name.equals(weapon.name)
-                && type == weapon.type;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(Weapon.class, name, damage, weight, type);
+        super(name, damage, weight);
     }
 
     @Override
     public String toString() {
-        return "Weapon{name='%s', damage=%d, weight=%d, type=%s}"
-                .formatted(name, damage, weight, type);
+        String fp = super.toString();
+        return type+fp;
     }
 }
