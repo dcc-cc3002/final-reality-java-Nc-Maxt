4afbd4536d60e412120dea33cf339d855ddf4dea
Nc-Maxt <m.f.nunez.caceres@gmail.com>
2022-09-28 14:38:19 -0300

    Change and add documentation in Knight

--- src/main/java/cl/uchile/dcc/finalreality/model/character/player/Knight.java
+++ src/main/java/cl/uchile/dcc/finalreality/model/character/player/Knight.java
@@ -1,12 +1,12 @@
+package cl.uchile.dcc.finalreality.model.character.player;
+
 /*
- * "Final Reality" (c) by R8V and ~Your name~
+ * "Final Reality" (c) by R8V and NM
  * "Final Reality" is licensed under a
  * Creative Commons Attribution 4.0 International License.
  * You should have received a copy of the license along with this
  * work. If not, see <http://creativecommons.org/licenses/by/4.0/>.
  */
-
-package cl.uchile.dcc.finalreality.model.character.player;
 
 import cl.uchile.dcc.finalreality.exceptions.InvalidStatValueException;
 import cl.uchile.dcc.finalreality.model.character.GameCharacter;
@@ -17,6 +17,9 @@ import org.jetbrains.annotations.NotNull;
 /**
  * A {@link PlayerCharacter} that can equip {@code Sword}s,{@code Knife}s and
  * {@code Axe}s.
+ *
+ * @author <a href="https://github.com/Nc-Maxt">NM</a>
+ * @author Matias Nunez
  */
 public class Knight extends AbstractPlayerCharacter {
 
@@ -38,15 +41,12 @@ public class Knight extends AbstractPlayerCharacter {
     super(name, maxHp, defense, turnsQueue);
   }
 
-  @Override
-  public String toString() {
-    return "Knight{maxHp=%d, defense=%d, name='%s'}".formatted(maxHp, defense, name);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(Knight.class, name, maxHp, defense);
-  }
+  /**
+   * Returns a boolean that indicates if 2 Knights are equals
+   *
+   * @param obj
+   *    the object that will be compared with "this"
+   */
 
   @Override
   public boolean equals(final Object obj) {
@@ -57,8 +57,24 @@ public class Knight extends AbstractPlayerCharacter {
       return false;
     }
     return hashCode() == that.hashCode()
-        && name.equals(that.name)
-        && maxHp == that.maxHp
-        && defense == that.defense;
+            && name.equals(that.name)
+            && maxHp == that.maxHp
+            && defense == that.defense;
+  }
+
+  /**
+   * return the Black Mage's hash number.
+   */
+  @Override
+  public int hashCode() {
+    return Objects.hash(Knight.class, name, maxHp, defense);
+  }
+
+  /**
+   * return a string with information about the Knight.
+   */
+  @Override
+  public String toString() {
+    return "Knight{maxHp=%d, defense=%d, name='%s'}".formatted(maxHp, defense, name);
   }
 }
