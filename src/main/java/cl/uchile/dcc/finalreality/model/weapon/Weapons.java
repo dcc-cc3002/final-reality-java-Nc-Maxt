08f611f4f15eb47b1acaaed99822fc0e1240ef81
Nc-Maxt <m.f.nunez.caceres@gmail.com>
2022-09-28 14:54:42 -0300

    Change and add documentation in Weapons

--- src/main/java/cl/uchile/dcc/finalreality/model/weapon/Weapons.java
+++ src/main/java/cl/uchile/dcc/finalreality/model/weapon/Weapons.java
@@ -4,16 +4,25 @@ package cl.uchile.dcc.finalreality.model.weapon;
  * An interface that set obligatory getters for the weapons
  *
  * @author <a href="https://github.com/Nc-Maxt">NM</a>
- * @author ~Matias Nunez~
+ * @author Matias Nunez
  */
 
 public interface Weapons {
+    // region : ACCESSORS
+
+    /**
+     * Returns this weapon's name.
+     */
     String getName();
 
+    /**
+     * Returns this weapons's damage.
+     */
     int getDamage();
 
+    /**
+     * Returns this weapons's weight.
+     */
     int getWeight();
-
-    String toString();
-
+    // endregion
 }
