efd9da30b71bb5835127610be77d0bf9ad500232
Nc-Maxt <m.f.nunez.caceres@gmail.com>
2022-09-30 15:04:39 -0300

    Add AbstractClass for getters and constructor of the Weapons

--- src/main/java/cl/uchile/dcc/finalreality/model/weapon/AbstractWeapon.java
+++ src/main/java/cl/uchile/dcc/finalreality/model/weapon/AbstractWeapon.java
@@ -1,2 +1,42 @@
-package cl.uchile.dcc.finalreality.model.weapon;public class AbstractWeapon {
+package cl.uchile.dcc.finalreality.model.weapon;
+
+public abstract class AbstractWeapon implements Weapons {
+
+    protected final String name;
+    protected final int damage;
+    protected final int weight;
+    /**
+     * Creates an Axe with a name, a base damage, and it's type.
+     */
+    protected AbstractWeapon(final String name, final int damage, final int weight) {
+        this.name = name;
+        this.damage = damage;
+        this.weight = weight;
+    }
+
+    // region : ACCESSORS
+
+    /**
+     * Returns the name of the weapon.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Returns the damage of the weapon.
+     */
+    public int getDamage() {
+        return damage;
+    }
+
+    /**
+     * Returns the weight of the weapon.
+     */
+    public int getWeight() {
+        return weight;
+    }
+
+    // endregion
+
 }
