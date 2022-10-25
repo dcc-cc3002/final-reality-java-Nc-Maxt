9fed5024e031f6d40b8ea9868e28210bec1e5703
Nc-Maxt <m.f.nunez.caceres@gmail.com>
2022-10-25 00:03:33 -0300

    Create new BlackMage test

--- src/test/java/cl/uchile/dcc/finalreality/model/character/player/BlackMageTest.java
+++ src/test/java/cl/uchile/dcc/finalreality/model/character/player/BlackMageTest.java
@@ -1,4 +1,22 @@
+package cl.uchile.dcc.finalreality.model.character.player;
+
+import cl.uchile.dcc.finalreality.model.weapon.StaffTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
-class BlackMageTest {
-  
+
+class BlackMageTest extends StaffTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void testEquals1() {
+    }
+
+    @Test
+    void testToString1() {
+    }
 }
