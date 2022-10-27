d85b0c0b818036b62382cf67dd354b24a46a5e56
Nc-Maxt <m.f.nunez.caceres@gmail.com>
2022-10-26 22:08:16 -0300

    Add Test methods for exceptions in the new TestClasses

--- src/test/java/cl/uchile/dcc/exceptions/InvalidStatValueExceptionTest.java
+++ src/test/java/cl/uchile/dcc/exceptions/InvalidStatValueExceptionTest.java
@@ -1,4 +1,7 @@
+package cl.uchile.dcc.exceptions;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class InvalidStatValueExceptionTest {
-  
+
 }
--- src/test/java/cl/uchile/dcc/exceptions/RequireTest.java
+++ src/test/java/cl/uchile/dcc/exceptions/RequireTest.java
@@ -1,4 +1,16 @@
+package cl.uchile.dcc.exceptions;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class RequireTest {
-  
+
+    @Test
+    void statValueAtLeast() {
+    }
+
+    @Test
+    void statValueAtMost() {
+    }
 }
