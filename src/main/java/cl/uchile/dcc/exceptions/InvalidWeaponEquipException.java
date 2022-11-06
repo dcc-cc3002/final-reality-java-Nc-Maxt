740560cb6faf11b6389a208703dab0570bd1c9a1
Nc-Maxt <m.f.nunez.caceres@gmail.com>
2022-11-05 21:35:09 -0300

    Create the new Exception

--- src/main/java/cl/uchile/dcc/exceptions/InvalidWeaponEquipException.java
+++ src/main/java/cl/uchile/dcc/exceptions/InvalidWeaponEquipException.java
@@ -1,2 +1,19 @@
-package cl.uchile.dcc.exceptions;public class InvalidWeaponEquipException {
+package cl.uchile.dcc.exceptions;
+
+
+/**
+ * This error is used to represent an invalid stat value.
+ *
+ * @author <a href="https://github.com/Nc-Maxt">NM</a>
+ * @author Matias Nunez
+ */
+
+public class InvalidWeaponEquipException  extends Exception {
+    /**
+     * Creates a new {@code InvalidStatValueException} with a {@code description} of the
+     * error.
+     */
+    public InvalidWeaponEquipException(String description) {
+        super("The provided Weapon is not a valid Weapon for this Class. " + description);
+    }
 }
