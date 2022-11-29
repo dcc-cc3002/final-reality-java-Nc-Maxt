1bf644665fca6e40e0f44dd3b28b5edb63aedbc1
Nc-Maxt <m.f.nunez.caceres@gmail.com>
2022-11-28 22:01:17 -0300

    Add new test for new exceptions in staff constructor

--- src/test/java/cl/uchile/dcc/finalreality/model/weapon/StaffTest.java
+++ src/test/java/cl/uchile/dcc/finalreality/model/weapon/StaffTest.java
@@ -1,13 +1,15 @@
 package cl.uchile.dcc.finalreality.model.weapon;
 
 
+import cl.uchile.dcc.finalreality.model.weapon.magical.Staff;
+import cl.uchile.dcc.finalreality.model.weapon.nomagical.Sword;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class StaffTest extends SwordTest {
-    private Staff st1 = new Staff("Demon invoquer", 3, 6);
-    private Staff st2 = new Staff("Bonk", 1000, 2);
-    protected Staff st3 = new Staff("Bonk", 1000, 2);
+    private Staff st1 = new Staff("Demon invoquer", 3, 4, 6);
+    private Staff st2 = new Staff("Bonk", 1000, 10, 2);
+    protected Staff st3 = new Staff("Bonk", 1000, 10, 2);
     private Sword s01 = new Sword("Greatsword", 34, 15);
     private Sword s11 = new Sword("Bonk", 1000, 2);
 
@@ -36,11 +38,17 @@ public class StaffTest extends SwordTest {
 
     @Test
     void testResolutionExceptioninConstructorsstaff() {
-        Staff st0_test = new Staff("Mcfly", -3, 64);
+        Staff st0_test = new Staff("Mcfly", -3, 10, 64);
         assertNotEquals(-3, st0_test.getDamage(), "Damage shouldn´t be negative even if given a negative to the constructor");
-        Staff st1_test = new Staff("Mccoffin", 0, 23);
+        Staff st1_test = new Staff("Mccoffin", 0, 5,23);
         assertEquals(0, st1_test.getDamage(), "Damage should be 0 if given 0 to the constructor");
-        Staff st2_test = new Staff("Lucy", 42, 32);
+        Staff st2_test = new Staff("Lucy", 42, 6, 32);
         assertEquals(42, st2_test.getDamage(), "Damage should be the same as the given to the constructor if it's possitive");
+        Staff st3_test = new Staff("Dondimadon", 10, -10, 64);
+        assertNotEquals(-10, st3_test.getMagicDamage(), "MagicDamage shouldn´t be lower than 1 even if given a lower number to the constructor");
+        Staff st4_test = new Staff("Mccoffin", 5, 1,23);
+        assertEquals(1, st4_test.getMagicDamage(), "MagicDamage should be 1 if given 1 to the constructor");
+        Staff st5_test = new Staff("magicbonk", 42, 53, 32);
+        assertEquals(53, st5_test.getMagicDamage(), "MagicDamage should be the same as the given to the constructor if it's upper to 1");
     }
 }
