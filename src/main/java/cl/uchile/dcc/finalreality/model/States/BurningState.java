f5b7747de5ce10fb663779f8375689bcfc4b47b1
Nc-Maxt <m.f.nunez.caceres@gmail.com>
2022-11-30 15:16:13 -0300

    Add constructor and override methods of the abstract classes in package states

--- src/main/java/cl/uchile/dcc/finalreality/model/States/BurningState.java
+++ src/main/java/cl/uchile/dcc/finalreality/model/States/BurningState.java
@@ -1,2 +1,13 @@
-package cl.uchile.dcc.finalreality.model.States;public class BurningState {
+package cl.uchile.dcc.finalreality.model.States;
+
+public class BurningState extends AbstractDmgState {
+  protected BurningState(int dmg) {
+    super(dmg);
+  }
+
+  @Override
+  public void toburn(int dmg) {error();}
+
+  @Override
+  public boolean isBurning() {return true;}
 }
