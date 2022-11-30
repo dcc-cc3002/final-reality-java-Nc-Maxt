79632c849bdca1229c5173663f47068ad44772d1
Nc-Maxt <m.f.nunez.caceres@gmail.com>
2022-11-30 15:18:32 -0300

    Add constructor and override methods of the abstract classes in package states

--- src/main/java/cl/uchile/dcc/finalreality/model/States/PoisonedState.java
+++ src/main/java/cl/uchile/dcc/finalreality/model/States/PoisonedState.java
@@ -1,2 +1,14 @@
-package cl.uchile.dcc.finalreality.model.States;public class PoisonedState {
+package cl.uchile.dcc.finalreality.model.States;
+
+public class PoisonedState extends AbstractDmgState {
+
+  protected PoisonedState(int dmg) {
+    super(dmg);
+  }
+
+  @Override
+  public void topoison(int dmg) {error();}
+
+  @Override
+  public boolean isPoisoned() {return true;}
 }
