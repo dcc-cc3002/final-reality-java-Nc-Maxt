f17c352cdd101df30103f2bd53406a607eb4c91b
Nc-Maxt <m.f.nunez.caceres@gmail.com>
2022-11-30 15:19:21 -0300

    Add methods that must be implemented in State classes

--- src/main/java/cl/uchile/dcc/finalreality/model/States/State.java
+++ src/main/java/cl/uchile/dcc/finalreality/model/States/State.java
@@ -1,4 +1,27 @@
 package cl.uchile.dcc.finalreality.model.States;
 
+import cl.uchile.dcc.finalreality.model.character.Enemy;
+import cl.uchile.dcc.finalreality.model.character.player.PlayerCharacter;
+
 public interface State {
+
+  void setChar(Enemy enem);
+
+  void stateattack(PlayerCharacter Pc, int damage);
+
+  void topoison(int dmg);
+
+  void toparalyze();
+
+  void tonormal();
+
+  void toburn(int dmg);
+
+  boolean isBurning();
+
+  boolean isParalyzed();
+
+  boolean isPoisoned();
+
+  boolean isNormal();
 }
