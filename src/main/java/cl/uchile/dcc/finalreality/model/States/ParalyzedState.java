39480effab803160077b3192807bdf6bbcc42504
Nc-Maxt <m.f.nunez.caceres@gmail.com>
2022-11-30 15:17:31 -0300

    Override methods of the abstract class State and add effect to the pj attack

--- src/main/java/cl/uchile/dcc/finalreality/model/States/ParalyzedState.java
+++ src/main/java/cl/uchile/dcc/finalreality/model/States/ParalyzedState.java
@@ -1,2 +1,18 @@
-package cl.uchile.dcc.finalreality.model.States;public class ParalyzedState {
+package cl.uchile.dcc.finalreality.model.States;
+
+import cl.uchile.dcc.finalreality.model.character.player.PlayerCharacter;
+
+public class ParalyzedState extends AbstractState{
+
+  protected ParalyzedState() {}
+
+  @Override
+  public void stateattack(PlayerCharacter Pc, int damage) { System.out.println("The Enemy is paralyzed, they can't attack right now"); }
+
+  @Override
+  public void toparalyze() {error();}
+
+  @Override
+  public boolean isParalyzed() {return true;}
+
 }
