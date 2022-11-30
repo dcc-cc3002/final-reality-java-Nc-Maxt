b497eb837b1ad9c2cf14d6f7243781d1f48c5a05
Nc-Maxt <m.f.nunez.caceres@gmail.com>
2022-11-30 15:15:07 -0300

    Add abstract class with constructor and new parameter to States that affects hp

--- src/main/java/cl/uchile/dcc/finalreality/model/States/AbstractDmgState.java
+++ src/main/java/cl/uchile/dcc/finalreality/model/States/AbstractDmgState.java
@@ -1,2 +1,15 @@
-package cl.uchile.dcc.finalreality.model.States;public class AbstractDmgState {
+package cl.uchile.dcc.finalreality.model.States;
+
+public abstract class AbstractDmgState extends AbstractState{
+  private int magicdmg;
+
+  protected AbstractDmgState(int dmg) {
+    double dmgxturn = (dmg/3);
+    this.magicdmg = (int) dmgxturn;
+  }
+
+  public int getMagicdmg() {
+    return magicdmg;
+  }
+
 }
