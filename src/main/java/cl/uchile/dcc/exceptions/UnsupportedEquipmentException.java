bdbdf9115594ce59d2cbf5cb0dbcf1c1c5f3ae1f
Nc-Maxt <m.f.nunez.caceres@gmail.com>
2022-12-03 21:18:22 -0300

    Delete exception: equipweapon, setspell and condense the structure in one class

--- src/main/java/cl/uchile/dcc/exceptions/UnsupportedEquipmentException.java
+++ src/main/java/cl/uchile/dcc/exceptions/UnsupportedEquipmentException.java
@@ -1,2 +1,25 @@
-package cl.uchile.dcc.exceptions;public class UnsupportedEquipmentException {
+package cl.uchile.dcc.exceptions;
+
+/**
+ * This error is used to represent a try to use an invalid weapon/spell to certain action.
+ *
+ * @author <a href="https://github.com/Nc-Maxt">NM</a>
+ * @author Matias Nunez
+ */
+
+public class UnsupportedEquipmentException extends Exception{
+  /**
+   * Creates a new {@code UnsupportedEquipmentException} with a {@code description} of the
+   * error.
+   *
+   * @param tip         the type of the equipment
+   * @param reason      complementary info
+   * @param description the description of the exception
+   */
+
+  public UnsupportedEquipmentException(String tip, String reason, String description) {
+      super(("The provided '%s' is not a valid '%s' for '%s'. "
+          .formatted(tip, tip, reason)) + description);
+  }
+
 }
