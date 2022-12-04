7ab7503b573c9a466967cf4c46eac3ba41ba7435
Nc-Maxt <m.f.nunez.caceres@gmail.com>
2022-12-03 21:45:34 -0300

    Create a Spell interface and add methods that every spell should implement

--- src/main/java/cl/uchile/dcc/finalreality/model/magic/Spell.java
+++ src/main/java/cl/uchile/dcc/finalreality/model/magic/Spell.java
@@ -1,2 +1,19 @@
-package cl.uchile.dcc.finalreality.model.magic;public interface Spell {
+package cl.uchile.dcc.finalreality.model.magic;
+
+import cl.uchile.dcc.exceptions.InvalidStatValueException;
+import cl.uchile.dcc.exceptions.UnsupportedEquipmentException;
+import cl.uchile.dcc.finalreality.model.character.GameCharacter;
+import cl.uchile.dcc.finalreality.model.character.player.Magicusers.BlackMage;
+import cl.uchile.dcc.finalreality.model.character.player.Magicusers.Mages;
+import cl.uchile.dcc.finalreality.model.character.player.Magicusers.WhiteMage;
+
+public interface Spell {
+
+  void useSpell(Mages mgs, GameCharacter gma, int mgdmg) throws InvalidStatValueException;
+
+  void BM_trytosetSpll(BlackMage BMmg) throws UnsupportedEquipmentException;
+
+  void WM_trytosetSpll(WhiteMage WMmg) throws UnsupportedEquipmentException;
+
+  int getManacost();
 }
