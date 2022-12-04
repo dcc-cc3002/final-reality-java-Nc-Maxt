7d4ee83e084db72caae236bcd1c815b5656097ca
Nc-Maxt <m.f.nunez.caceres@gmail.com>
2022-12-03 21:34:01 -0300

    Create constructor and add getter

--- src/main/java/cl/uchile/dcc/finalreality/model/magic/AbstractSpell.java
+++ src/main/java/cl/uchile/dcc/finalreality/model/magic/AbstractSpell.java
@@ -1,2 +1,15 @@
-package cl.uchile.dcc.finalreality.model.magic;public class AbstractSpell {
+package cl.uchile.dcc.finalreality.model.magic;
+
+public abstract class AbstractSpell implements Spell {
+  private int Manacost;
+
+  protected AbstractSpell (int cost) {
+    this.Manacost = cost;
+  }
+
+  public int getManacost() {
+    return Manacost;
+  }
+
+
 }
