f63a072e162f94f123022aeac39ed0f0f7433236
Nc-Maxt <m.f.nunez.caceres@gmail.com>
2022-12-03 21:44:16 -0300

    Create constructors, and Add castSpell method
    the effect depends on the Spell and characteristics

--- src/main/java/cl/uchile/dcc/finalreality/model/magic/BMgc/FireSpell.java
+++ src/main/java/cl/uchile/dcc/finalreality/model/magic/BMgc/FireSpell.java
@@ -1,4 +1,18 @@
 package cl.uchile.dcc.finalreality.model.magic.BMgc;
 
-public class FireSpell {
+
+import cl.uchile.dcc.finalreality.model.character.GameCharacter;
+
+public class FireSpell extends AbstractBMSpell{
+  public FireSpell() {
+    super(81);
+  }
+
+  @Override
+  public void castspell(GameCharacter gmcha, int mgdmg, int numalea) {
+    if (numalea>getProbrequired()) {
+      gmcha.Burn(mgdmg);
+    }
+    gmcha.reduceHp(mgdmg);
+  }
 }
--- src/main/java/cl/uchile/dcc/finalreality/model/magic/BMgc/ThunderSpell.java
+++ src/main/java/cl/uchile/dcc/finalreality/model/magic/BMgc/ThunderSpell.java
@@ -1,4 +1,18 @@
 package cl.uchile.dcc.finalreality.model.magic.BMgc;
 
-public class ThunderSpell {
+import cl.uchile.dcc.finalreality.model.character.GameCharacter;
+
+public class ThunderSpell extends AbstractBMSpell{
+  public ThunderSpell() {
+    super(71);
+  }
+
+  @Override
+  public void castspell(GameCharacter gmcha, int mgdmg, int numalea) {
+    if (numalea>getProbrequired()) {
+      gmcha.Thunder(mgdmg);
+    }
+    gmcha.reduceHp(mgdmg);
+  }
+
 }
