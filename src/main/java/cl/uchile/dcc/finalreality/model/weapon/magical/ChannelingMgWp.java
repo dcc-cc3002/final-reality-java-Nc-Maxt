13664e8f672c32ff023a0652ca62a5a2d8bf9964
Nc-Maxt <m.f.nunez.caceres@gmail.com>
2022-12-03 21:55:10 -0300

    New interface that agroups weapons that can be used in with magic

--- src/main/java/cl/uchile/dcc/finalreality/model/weapon/magical/ChannelingMgWp.java
+++ src/main/java/cl/uchile/dcc/finalreality/model/weapon/magical/ChannelingMgWp.java
@@ -1,2 +1,6 @@
-package cl.uchile.dcc.finalreality.model.weapon.magical;public interface ChannelingMgWp {
+package cl.uchile.dcc.finalreality.model.weapon.magical;
+
+import cl.uchile.dcc.finalreality.model.weapon.Weapons;
+
+public interface ChannelingMgWp extends Weapons {
 }
