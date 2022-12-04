b42b0b7f4e585992ce0365576b3112779a342c37
Nc-Maxt <m.f.nunez.caceres@gmail.com>
2022-12-03 21:41:41 -0300

    Implement DD methods, and add particular methods depending on the magic that can use each mage class

--- src/main/java/cl/uchile/dcc/finalreality/model/magic/BMgc/AbstractBMSpell.java
+++ src/main/java/cl/uchile/dcc/finalreality/model/magic/BMgc/AbstractBMSpell.java
@@ -1,2 +1,43 @@
-package cl.uchile.dcc.finalreality.model.magic.BMgc;public class AbstractBMSpell {
+package cl.uchile.dcc.finalreality.model.magic.BMgc;
+
+import cl.uchile.dcc.exceptions.UnsupportedEquipmentException;
+import cl.uchile.dcc.finalreality.model.character.GameCharacter;
+import cl.uchile.dcc.finalreality.model.character.player.Magicusers.BlackMage;
+import cl.uchile.dcc.finalreality.model.character.player.Magicusers.Mages;
+import cl.uchile.dcc.finalreality.model.character.player.Magicusers.WhiteMage;
+import cl.uchile.dcc.finalreality.model.magic.AbstractSpell;
+import cl.uchile.dcc.finalreality.model.magic.interfaceddmg.BlackMagic;
+
+import java.util.Random;
+
+public abstract class AbstractBMSpell extends AbstractSpell implements BlackMagic {
+  private final int probrequired;
+  protected AbstractBMSpell( int reqpro) {
+    super(15);
+    this.probrequired = reqpro;
+  }
+
+  protected int getProbrequired() {
+    return probrequired;
+  }
+
+  @Override
+  public void useSpell(Mages mgs, GameCharacter Gmcha, int mgdmg) {
+    Random rand = new Random();
+    int numalea = rand.nextInt(100)+1;
+    castspell(Gmcha, mgdmg, numalea);
+  }
+
+  abstract public void castspell(GameCharacter gmcha, int mgdmg, int numalea);
+
+
+  @Override
+  public void BM_trytosetSpll(BlackMage BMmg) {
+    BMmg.equipblackmagic(this);
+  }
+
+  @Override
+  public void WM_trytosetSpll(WhiteMage WMmg) throws UnsupportedEquipmentException {
+    throw new UnsupportedEquipmentException("Spell", "this Class", "Tried to equip a White Mage Spell");
+  }
 }
--- src/main/java/cl/uchile/dcc/finalreality/model/magic/WMgc/AbstractWMSpell.java
+++ src/main/java/cl/uchile/dcc/finalreality/model/magic/WMgc/AbstractWMSpell.java
@@ -1,2 +1,24 @@
-package cl.uchile.dcc.finalreality.model.magic.WMgc;public class AbstractWMSpell {
+package cl.uchile.dcc.finalreality.model.magic.WMgc;
+
+import cl.uchile.dcc.exceptions.UnsupportedEquipmentException;
+import cl.uchile.dcc.finalreality.model.character.player.Magicusers.BlackMage;
+import cl.uchile.dcc.finalreality.model.character.player.Magicusers.WhiteMage;
+import cl.uchile.dcc.finalreality.model.magic.AbstractSpell;
+import cl.uchile.dcc.finalreality.model.magic.interfaceddmg.WhiteMagic;
+
+public abstract class AbstractWMSpell extends AbstractSpell implements WhiteMagic {
+
+  protected AbstractWMSpell(int cost) {
+    super(cost);
+  }
+
+  @Override
+  public void BM_trytosetSpll(BlackMage BMmg) throws UnsupportedEquipmentException {
+    throw new UnsupportedEquipmentException("Spell", "this Class", "Tried to equip a Black Mage Spell");
+  }
+
+  @Override
+  public void WM_trytosetSpll(WhiteMage WMmg) {
+    WMmg.equipwhitemagic(this);
+  }
 }
