68749658db78c9b88c05725ccea84946d297dbd5
Nc-Maxt <m.f.nunez.caceres@gmail.com>
2022-12-05 23:22:22 -0300

    Implement a first version of all the recommended methods

--- src/main/java/cl/uchile/dcc/GameController.java
+++ src/main/java/cl/uchile/dcc/GameController.java
@@ -4,68 +4,106 @@ import cl.uchile.dcc.exceptions.InvalidStatValueException;
 import cl.uchile.dcc.exceptions.UnsupportedEquipmentException;
 import cl.uchile.dcc.finalreality.model.character.Enemy;
 import cl.uchile.dcc.finalreality.model.character.GameCharacter;
+import cl.uchile.dcc.finalreality.model.character.player.Engineer;
+import cl.uchile.dcc.finalreality.model.character.player.Knight;
+import cl.uchile.dcc.finalreality.model.character.player.Magicusers.BlackMage;
+import cl.uchile.dcc.finalreality.model.character.player.Magicusers.WhiteMage;
 import cl.uchile.dcc.finalreality.model.character.player.PlayerCharacter;
+import cl.uchile.dcc.finalreality.model.character.player.Thief;
 import cl.uchile.dcc.finalreality.model.weapon.Weapons;
 
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
-import java.util.random.RandomGenerator;
 
 public class GameController {
 
   private final BlockingQueue<GameCharacter> queue1 = new LinkedBlockingQueue<>();
   private final ArrayList<PlayerCharacter> listapc = new ArrayList<>() ;
   private final ArrayList<Enemy> listaen = new ArrayList<>() ;
-  private final Random seed;
+  private final int seed;
+  private Random generator;
 
   public GameController() {
-    this(new Random());
+    this(new Random().nextInt());
   }
 
-  public GameController(Random kernel) {
+  public GameController(int kernel) {
     this.seed = kernel;
-    //for(int i = 1; i <= (seed.nextInt(2)+5); i = i + 1){
-
-      // TODO: Add enemies to the game
-      // TODO: Add players to the game
-      // TODO: Add characters to the turns queue
-    //}
+    this.generator = new Random(kernel);
+    for(int i = 1; i <= (generator.nextInt(2)+5); i = i + 1) {
+      Enemy en = createEnemy("Enemy"+Integer.toString(i));
+      listaen.add(en);
+      en.waitTurn();
+    }
+    for (int i = 1; i <= 5; i = i+1) {
+      int clss = generator.nextInt(5)+1;
+      PlayerCharacter Pc = createPC(clss);
+      listapc.add(Pc);
+      Pc.waitTurn();
+    }
   }
 
-  public RandomGenerator getseed() {
+  private PlayerCharacter createPC(int clss) {
+    if (clss==1) {return createEngineer(Integer.toString(clss)); }
+    if (clss==2) {return createKnight(Integer.toString(clss)); }
+    if (clss==3) {return createThief(Integer.toString(clss)); }
+    if (clss==4) {return createWhiteMage(Integer.toString(clss)); }
+    return createBlackMage(Integer.toString(clss));
+  }
+
+  public int getseed() {
     return seed;
   }
 
 
-
-
-  public void createEngineer(String name , int hp, int defense, Weapons weapon) {
-    // TODO: Create a player character
+  public Engineer createEngineer(String name) {
+    int maxHp = generator.nextInt(40)+200;
+    int def = generator.nextInt(30)+90;
+    return new Engineer(name, maxHp, def, queue1);
     // hp = 200, def 70
+    // , Weapons weapon
   }
-  public void createKnight(String name , int hp, int defense, Weapons weapon) {
-    // TODO: Create a player character
+  public Knight createKnight(String name) {
+    int maxHp = generator.nextInt(80)+300;
+    int def = generator.nextInt(25)+150;
+    return new Knight(name, maxHp, def, queue1);
     // hp = 300, def 150
+    // , Weapons weapon
 
   }
-  public void createThief(String name , int hp, int defense, Weapons weapon) {
-    // TODO: Create a player character
+  public Thief createThief(String name) {
+    int maxHp = generator.nextInt(40)+160;
+    int def = generator.nextInt(35)+25;
+    return new Thief(name, maxHp, def, queue1);
     // hp 250, def 25
+    // , Weapons weapon
   }
-  public void createBlackMage(String name , int hp, int defense, Weapons weapon) {
-    // TODO: Create a player character
+  public BlackMage createBlackMage(String name) {
+    int maxMp = generator.nextInt(60)+70;
+    int maxHp = generator.nextInt(40)+80;
+    int def = generator.nextInt(30)+80;
+    return new BlackMage(name, maxHp, def, maxMp, queue1);
     // hp 100, def 50, mp 70
+    // , Weapons weapon
   }
 
-  public void createWhiteMage(String name , int hp, int defense, Weapons weapon) {
-    // TODO: Create a player character
+  public WhiteMage createWhiteMage(String name) {
+    int maxMp = generator.nextInt(50)+80;
+    int maxHp = generator.nextInt(40)+120;
+    int def = generator.nextInt(30)+60;
+    return new WhiteMage(name, maxHp, def, maxMp, queue1);
+    // Falta añadir el arma y crearla
     // hp 100, def 50, mp 80
   }
 
-  public void createEnemy(String name, int hp, int defense, int weight) {
-    // TODO: Create an enemy character
+  public Enemy createEnemy(String name) {
+    int weight = generator.nextInt(30)+50;
+    int maxHp = generator.nextInt(100)+500;
+    int def = generator.nextInt(50)+80;
+    int attack = generator.nextInt(30)+20;
+    return new Enemy(name , weight, maxHp, def, attack ,queue1);
     // hp 500, def 100, weight 50
   }
 
@@ -79,14 +117,27 @@ public class GameController {
 
   public void waitTurn(GameCharacter character) {
     character.waitTurn();
-    // TODO: Call the waitTurn method of the character
   }
 
-  public void onPlayerWin() {
+  public boolean onPlayerWin() {
+    boolean Pwin = true;
+    for (Enemy enemy : listaen) {
+      if (enemy.getCurrentHp() != 0) {
+        Pwin = false;
+      }
+    }
+    return Pwin;
     // TODO: Handle the player winning the game
   }
 
-  public void onEnemyWin() {
+  public boolean onEnemyWin() {
+    boolean Ewin = true;
+    for (PlayerCharacter PC : listapc) {
+      if (PC.getCurrentHp() != 0) {
+        Ewin = false;
+      }
+    }
+    return Ewin;
     // TODO: Handle the enemy winning the game
   }
 
