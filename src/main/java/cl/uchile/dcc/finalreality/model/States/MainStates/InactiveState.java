79dc12aff3c12bcd27df0967fe240ba1d9a8d68e
Nc-Maxt <m.f.nunez.caceres@gmail.com>
2022-12-10 12:16:57 -0300

    Implement Inactive main state

--- src/main/java/cl/uchile/dcc/finalreality/model/States/MainStates/InactiveState.java
+++ src/main/java/cl/uchile/dcc/finalreality/model/States/MainStates/InactiveState.java
@@ -1,4 +1,20 @@
-package cl.uchile.dcc.finalreality.Controller.MainStates;
+package cl.uchile.dcc.finalreality.model.States.MainStates;
 
-public class Inactive {
+public class InactiveState extends AbstractMainState {
+
+  @Override
+  public void toactive() {
+    this.changeState(new ActiveState());
+  }
+
+
+  @Override
+  public void todead() {
+    this.changeState(new DeadState());
+  }
+
+  @Override
+  public boolean isInactive() {
+    return true;
+  }
 }
