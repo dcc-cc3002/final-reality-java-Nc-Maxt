eb54728e540abd8891419370e954729f4dfb2f52
Nc-Maxt <m.f.nunez.caceres@gmail.com>
2022-12-10 12:15:37 -0300

    Implement Active main state

--- src/main/java/cl/uchile/dcc/finalreality/model/States/MainStates/ActiveState.java
+++ src/main/java/cl/uchile/dcc/finalreality/model/States/MainStates/ActiveState.java
@@ -1,4 +1,25 @@
-package cl.uchile.dcc.finalreality.Controller.MainStates;
+package cl.uchile.dcc.finalreality.model.States.MainStates;
 
-public class Active {
+public class ActiveState extends AbstractMainState {
+
+  @Override
+  public void toactive() {
+    this.changeState(new ActiveState());
+  }
+
+  @Override
+  public void toinactive() {
+    this.changeState(new InactiveState());
+  }
+
+  @Override
+  public void todead() {
+    this.changeState(new DeadState());
+  }
+
+  @Override
+  public boolean isActive() {
+    return true;
+  }
+
 }
